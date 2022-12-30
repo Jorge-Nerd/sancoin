5802aadc74968bf83a4682f2bc9f3d9011ef06fe
Jail_IB <jailsonsanches9@gmail.com>
2022-12-30 21:08:34 +0100

    feat:popular

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Veggy from '../components/Veggy'
+import Popular from '../components/Popular'
+
+
+const Home = () => {
+  return (
+    <div>
+        <Veggy />
+        <Popular />
+    </div>
+  )
+}
+
+export default Home
